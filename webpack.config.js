63f4b04fef38d95a417a29a87e5adbe021aba9ad
dongwengao <1241287226@qq.com>
2024-10-14 00:17:11 +0800

    second commit

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
-    mode: 'none',
+    mode: 'development',
     entry: './src/main.js',
     output: {
         filename: 'bundle.js',
